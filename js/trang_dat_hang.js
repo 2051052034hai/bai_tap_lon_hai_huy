ef2d14ac584af974faf1a045e24eb8707feb1961
2051052034hai <91556828+2051052034hai@users.noreply.github.com>
2021-10-01 00:38:54 +0700

    sửa bài lần cuối
    sửa bài

--- js/trang_dat_hang.js
+++ js/trang_dat_hang.js
@@ -1,4 +1,101 @@
 
+  /* phần js của trang đặt hàng */
+  function init(){
+      /*  Javascrip xử lý hiệu ứng khi cick vào ảnh sản phẩm */
+      var images2 = document.querySelectorAll(".picture img")
+      for(var i = 0; i < images2.length; i++)
+      images2[i].onclick = function() {
+        var path = this.src
+        var img = document.getElementById("mainimage")
+        img.src = path
+      }
+
+
+      /*/////////// js của trang liên hệ/////////////// */
+      function Validator(options){
+        // Định nghĩa hàm validate
+        function validate(inputElement, rule){
+          var errorMessage = rule.test(inputElement.value);
+          var errorElement = inputElement.parentElement.querySelector(options.errorSelector);
+            
+          if(errorMessage){
+            errorElement.innerText = errorMessage;
+            inputElement.parentElement.classList.add('invalid')
+            inputElement.classList.add('input-error')
+        }else{
+            errorElement.innerText='';
+            inputElement.parentElement.classList.remove('invalid')
+            inputElement.classList.remove('input-error')
+          }
+        }
+  
+        // lấy emlement của form cần validate
+        var formElement = document.querySelector(options.form);
+        if(formElement){
+          /* Lặp qua từng rule và thực hiện Validate */
+          formElement.onsubmit = function(e){
+            e.preventDefault();
+            options.rules.forEach(function(rule){
+              var inputElement = formElement.querySelector(rule.selector);
+                validate(inputElement,rule);
+            });
+         }
+
+           options.rules.forEach(function(rule){
+            var inputElement = formElement.querySelector(rule.selector);
+            if(inputElement){
+              
+                // xử lý trường hợp blur khỏi input
+                inputElement.onblur = function(){
+                 validate(inputElement,rule);
+                }
+                // xử lý mỗi khi người dùng nhập vào input
+                inputElement.oninput = function(){
+                  var errorElement = inputElement.parentElement.querySelector(options.errorSelector);
+                  errorElement.innerText='';
+                  inputElement.parentElement.classList.remove('invalid')
+                  inputElement.classList.remove('input-error')
+                }
+              }
+           })
+        }
+    }
+    // Nguyên tắc của các rules
+    // 1.khi có lỗi ==> trả ra messae lỗi
+    // 2.Khi hợp lệ ==> trả về undefined
+    Validator.isRequired = function (selector){
+        return {
+            selector:selector,
+            test:function(value){
+                return value.trim() ? undefined: "Vui lòng nhập trường hợp này?"
+            }
+        };
+    }
+    // Kiểm tra xem người dùng có phải nhập email hay khônng
+    Validator.isEmail = function (selector){
+        return {
+            selector:selector,
+            test:function(value){
+                var regex =/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+                return regex.test(value) ? undefined: "Trường này phải là Email"
+            }
+        };
+    }
+    // output nhận được
+    Validator({
+        form: '#form-1',
+        errorSelector:'.form-message',
+        rules:[
+            Validator.isRequired('#fullname'),
+            Validator.isEmail('#email'),
+            Validator.isRequired('#chu-de'),
+            Validator.isRequired('#noi-dung'),
+        ]
+    });
+  }
+
+
+  ///////////PHẦN JQUERY CỦA TRANG ĐẶT HÀNG /////////////////*/
   /* Jquery định dạng cho thanh số lượng sản phẩm */
   $(document).ready(function(){
     var $this = $("input.input-qty");
